4e07b733f7b922c30f7d236f8f091e24b837628a
zach pratt <zach@modzed.io>
2020-02-10 22:40:30 -0600

    Remove call to mockery service

--- ui/src/namespaces/namespace-repository.js
+++ ui/src/namespaces/namespace-repository.js
@@ -1,7 +1,6 @@
 export const findAllNamespaces = () => {
     return (dispatch) => {
-        const namespaceUrl = 'http://www.mocky.io/v2/5e40d7162f00005500583077';
-        // const namespaceUrl = '/api';
+        const namespaceUrl = '/api';
 
         fetch(namespaceUrl)
             .then((result) => {
