2a45e38ea4591debf69906a3ade3df572ce7320d
zach pratt <zach@modzed.io>
2021-12-26 16:19:45 -0600

    get the UI working with e2e tls
    and upgrade nextjs to v11

--- ui/next.config.js
+++ ui/next.config.js
@@ -1,6 +1,6 @@
-const isProd = process.env.NODE_ENV === 'production'
+const productionPath = '/ui';
 
 module.exports = {
-    assetPrefix: isProd ? '/ui' : '',
-    publicRuntimeConfig: '/ui'
-}
+    assetPrefix: `${productionPath}/`,
+    basePath: productionPath
+};
