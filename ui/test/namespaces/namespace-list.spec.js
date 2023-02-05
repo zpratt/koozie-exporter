2be53fd3ea1cb51f5dd0590a1e87632c21513435
zach pratt <zach@modzed.io>
2023-02-04 21:15:47 -0600

    chore: assert that findAllNamespaces is used

--- ui/test/namespaces/namespace-list.spec.js
+++ ui/test/namespaces/namespace-list.spec.js
@@ -1,8 +1,9 @@
 import {Provider} from "react-redux";
 import NamespaceListContainer from '../../src/namespaces/namespace-list';
-import {beforeEach, describe, it, expect} from '@jest/globals';
-import {render} from '@testing-library/react';
+import {afterEach, beforeEach, describe, expect, it} from '@jest/globals';
+import {cleanup, render} from '@testing-library/react';
 import Chance from 'chance';
+import {findAllNamespaces} from "../../src/namespaces/namespace-repository";
 
 jest.mock('../../src/namespaces/namespace-repository');
 
@@ -16,6 +17,14 @@ function mockStoreWithState(state) {
     };
 }
 
+function givenANameSpaceList(namespace) {
+    return {
+        namespaces: [{
+            name: namespace
+        }]
+    };
+}
+
 describe('NamespaceList', () => {
     let chance;
 
@@ -23,19 +32,34 @@ describe('NamespaceList', () => {
         chance = new Chance();
     });
 
+    afterEach(() => {
+        cleanup();
+        jest.resetAllMocks();
+    });
+
     it('should show a list of namespaces from the cluster', () => {
         const firstNamespace = chance.word();
-        const expectedNamespaces = [{
-            name: firstNamespace
-        }];
+        const expectedNamespaces = givenANameSpaceList(firstNamespace);
         const store = mockStoreWithState({
-            app: {
-                namespaces: expectedNamespaces
-            }
-        })
+            app: expectedNamespaces
+        });
         const namespaceListEl = render(<Provider store={store}><NamespaceListContainer/></Provider>);
         namespaceListEl.container.querySelectorAll('div > div').forEach((namespaceEl) => {
             expect(namespaceEl.textContent).toContain(firstNamespace);
         });
+    });
+
+    it('should use the namespace repository to find all namespaces', async () => {
+        const store = mockStoreWithState({
+            app: {
+                namespaces: []
+            }
+        });
+        findAllNamespaces.mockReturnValueOnce([1, 2, 3]);
+        expect(findAllNamespaces).not.toHaveBeenCalled();
+
+        render(<Provider store={store}><NamespaceListContainer/></Provider>);
+
+        expect(findAllNamespaces).toHaveBeenCalled();
     });
 });
